df4a1e2e20931b5a45d97776eaceba4fd9468f8e
Markus Rauhalahti <mrauhalahti@gmail.com>
2019-08-05 14:05:54 +0300

    Used black to reformat, got rid of two space indents

--- GB_GA.py
+++ GB_GA.py
@@ -1,7 +1,7 @@
-'''
+"""
 Written by Jan H. Jensen 2018. 
 Many subsequent changes inspired by https://github.com/BenevolentAI/guacamol_baselines/tree/master/graph_ga
-'''
+"""
 
 from rdkit import Chem
 from rdkit.Chem import AllChem
@@ -9,7 +9,8 @@ from rdkit.Chem import Descriptors
 from rdkit.Chem import rdmolops
 
 from rdkit import rdBase
-rdBase.DisableLog('rdApp.error')
+
+rdBase.DisableLog("rdApp.error")
 
 import numpy as np
 import random
@@ -18,90 +19,106 @@ import sys
 
 import crossover as co
 import mutate as mu
-#import string_crossover as co
-#import string_mutate as mu
+
+# import string_crossover as co
+# import string_mutate as mu
 import scoring_functions as sc
 
+
 def read_file(file_name):
-  mol_list = []
-  with open(file_name,'r') as file:
-    for smiles in file:
-      mol_list.append(Chem.MolFromSmiles(smiles))
+    mol_list = []
+    with open(file_name, "r") as file:
+        for smiles in file:
+            mol_list.append(Chem.MolFromSmiles(smiles))
 
-  return mol_list
+    return mol_list
 
-def make_initial_population(population_size,file_name):
-  mol_list = read_file(file_name)
-  population = []
-  for i in range(population_size):
-    population.append(random.choice(mol_list))
-    
-  return population
+
+def make_initial_population(population_size, file_name):
+    mol_list = read_file(file_name)
+    population = []
+    for i in range(population_size):
+        population.append(random.choice(mol_list))
+
+    return population
+
 
 def calculate_normalized_fitness(scores):
-  sum_scores = sum(scores)
-  normalized_fitness = [score/sum_scores for score in scores]
+    sum_scores = sum(scores)
+    normalized_fitness = [score / sum_scores for score in scores]
 
-  return normalized_fitness
+    return normalized_fitness
 
-def make_mating_pool(population,fitness,mating_pool_size):
-  mating_pool = []
-  for i in range(mating_pool_size):
-  	mating_pool.append(np.random.choice(population, p=fitness))
 
-  return mating_pool
- 
+def make_mating_pool(population, fitness, mating_pool_size):
+    mating_pool = []
+    for i in range(mating_pool_size):
+        mating_pool.append(np.random.choice(population, p=fitness))
 
-def reproduce(mating_pool,population_size,mutation_rate):
-  new_population = []
-  while len(new_population) < population_size:
-    parent_A = random.choice(mating_pool)
-    parent_B = random.choice(mating_pool)
-    new_child = co.crossover(parent_A,parent_B)
-    if new_child != None:
-	    new_child = mu.mutate(new_child,mutation_rate)
-	    if new_child != None:
-	    	new_population.append(new_child)
+    return mating_pool
 
-  return new_population
 
-def sanitize(population,scores,population_size, prune_population):
+def reproduce(mating_pool, population_size, mutation_rate):
+    new_population = []
+    while len(new_population) < population_size:
+        parent_A = random.choice(mating_pool)
+        parent_B = random.choice(mating_pool)
+        new_child = co.crossover(parent_A, parent_B)
+        if new_child != None:
+            new_child = mu.mutate(new_child, mutation_rate)
+            if new_child != None:
+                new_population.append(new_child)
+
+    return new_population
+
+
+def sanitize(population, scores, population_size, prune_population):
     if prune_population:
-      smiles_list = []
-      population_tuples = []
-      for score, mol in zip(scores,population):
-          smiles = Chem.MolToSmiles(mol)
-          if smiles not in smiles_list:
-              smiles_list.append(smiles)
-              population_tuples.append((score,mol))
+        smiles_list = []
+        population_tuples = []
+        for score, mol in zip(scores, population):
+            smiles = Chem.MolToSmiles(mol)
+            if smiles not in smiles_list:
+                smiles_list.append(smiles)
+                population_tuples.append((score, mol))
     else:
-      population_tuples = list(zip(scores,population))
+        population_tuples = list(zip(scores, population))
 
-    population_tuples = sorted(population_tuples, key=lambda x: x[0], reverse=True)[:population_size]
+    population_tuples = sorted(population_tuples, key=lambda x: x[0], reverse=True)[
+        :population_size
+    ]
     new_population = [t[1] for t in population_tuples]
     new_scores = [t[0] for t in population_tuples]
 
     return new_population, new_scores
 
+
 def GA(args):
-  population_size, file_name,scoring_function,generations,mating_pool_size,mutation_rate, \
-  scoring_args, max_score, prune_population = args
+    population_size, file_name, scoring_function, generations, mating_pool_size, mutation_rate, scoring_args, max_score, prune_population = (
+        args
+    )
 
-  population = make_initial_population(population_size,file_name)
-  scores = sc.calculate_scores(population,scoring_function,scoring_args)
-  fitness = calculate_normalized_fitness(scores)
-
-  for generation in range(generations):
-    mating_pool = make_mating_pool(population,fitness,mating_pool_size)
-    new_population = reproduce(mating_pool,population_size,mutation_rate)
-    new_scores = sc.calculate_scores(new_population,scoring_function,scoring_args)
-    population, scores = sanitize(population+new_population, scores+new_scores, population_size, prune_population)  
+    population = make_initial_population(population_size, file_name)
+    scores = sc.calculate_scores(population, scoring_function, scoring_args)
     fitness = calculate_normalized_fitness(scores)
-    if scores[0] >= max_score:
-      break
 
-  return (scores, population, generation+1)
+    for generation in range(generations):
+        mating_pool = make_mating_pool(population, fitness, mating_pool_size)
+        new_population = reproduce(mating_pool, population_size, mutation_rate)
+        new_scores = sc.calculate_scores(new_population, scoring_function, scoring_args)
+        population, scores = sanitize(
+            population + new_population,
+            scores + new_scores,
+            population_size,
+            prune_population,
+        )
+        fitness = calculate_normalized_fitness(scores)
+        if scores[0] >= max_score:
+            break
 
+    return (scores, population, generation + 1)
 
+ 
+    
 if __name__ == "__main__":
     pass
